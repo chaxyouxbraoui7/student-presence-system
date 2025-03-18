95288ee074e529ed5522aa3a9aa33ec871b9e70e
Mednsn <mossmoss@gmail.com>
2025-03-18 16:04:13 +0000

    add comment

--- gui/main.py
+++ gui/main.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from gui.upload_csv import upload_csv  # Use absolute import instead of relative
 
-# general func
+# geeneral func
 
 def start_program():
     root = tk.Tk()
