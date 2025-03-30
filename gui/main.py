55f3607ed5e45b160ce2a5ed254123f453a21d13
Chabraoui Ayoub <ayoubchabraouiformal@gmail.com>
2025-03-30 17:41:18 +0000

    enhancement

--- gui/main.py
+++ gui/main.py
@@ -1,23 +1,41 @@
+# Importing all the necessary libraries & absolute importing of functions and variables
+
 import tkinter as tk
-from tkinter import messagebox
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from gui.upload_csv import upload_csv  # Use absolute import instead of relative
-#my branch
 
-# geeneral func
+# Adding the parent directory to the system path for module importing
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+# Importing the 'upload_csv' function from the 'upload_csv' file in the 'gui' folder
+
+from gui.upload_csv import upload_csv
+
+# The function that initializes and starts the application
 
 def start_program():
+    
+    # Creating the main app window
+    
     root = tk.Tk()
-    root.title("CSV & Image Upload")
-    root.geometry("400x200")
 
-    label = tk.Label(root, text="Upload your CSV file", font=("Arial", 12))
-    label.pack(pady=20)
+    # Setting the window's title
+    
+    root.title("Student • Presence • System")
+    
+    # Calling the 'upload_csv' function to set up the CSV upload interface
+    
+    upload_csv(root)
 
-    button = tk.Button(root, text="Select CSV File", command=upload_csv)
-    button.pack()
+    # Starting the tkinter event loop to keep the app interactive
+    
+    root.mainloop()
+
+# Checking if the script is being executed directly
 
 if __name__ == "__main__":
+    
+    # If true, the function can be called to launch the application
+    
     start_program()
